ad4bf633554ea9218fcd91fb51fc5c97d1e00cdd
Konrad Pagacz <konrad.pagacz@gmail.com>
2020-05-13 23:36:34 +0200

    spoj-2

--- polish/KOTYYYYY/solution.c
+++ polish/KOTYYYYY/solution.c
@@ -12,11 +12,10 @@ int main(void) {
     for(int i = 0; i < tests; i++) {
         scanf("%li %li %li %li", &first_age, &first_position, &second_age, &second_position);
         positions_between = abs(second_position - first_position) - 1;
-        printf("%li ", positions_between);
         if(positions_between % 3 == 0) {
-            (first_age > second_age) ? printf("0\n") : printf("-1\n");
+            (first_age > second_age) ? printf("0\n") : printf("1\n");
         } else {
-            (first_age > second_age) ? printf("-1\n") : printf("0\n");
+            (first_age > second_age) ? printf("1\n") : printf("0\n");
         }
 
     }
