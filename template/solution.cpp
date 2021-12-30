d707fc7d1f1575ac5870e77340d6f4083215c5c5
kpagacz <konrad.pagacz@gmail.com>
2021-12-30 01:51:49 +0100

    cpp template with makefile

---
+++ template/solution.cpp
@@ -0,0 +1,14 @@
+// link to the problem:
+#include<iostream>
+#include<vector>
+#include<array>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<sstream>
+#include<iterator>
+
+
+int main(int argc, char** argv) {
+
+}
