4a48646c7e0cbdde4243bf62bed0b069e504f17c
kpagacz <konrad.pagacz@gmail.com>
2022-01-01 12:46:29 +0100

    leetcode-build-array-from-permutation

---
+++ leetcode/build-array-from-permutation/solution.cpp
@@ -0,0 +1,23 @@
+// link to the problem:
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> buildArray(std::vector<int>& nums) {
+    std::vector<int> answer(nums.size());
+    for (int i = 0; i < nums.size(); i++) {
+      answer[i] = nums[nums[i]];
+    }
+    return answer;
+  }
+};
+
+int main(int argc, char** argv) {}
