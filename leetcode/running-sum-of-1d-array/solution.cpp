efa8cb21f275032db843a85c1124d6f2859562d4
kpagacz <konrad.pagacz@gmail.com>
2022-01-02 00:25:50 +0100

    leetcode-running-sum-of-1d-array

---
+++ leetcode/running-sum-of-1d-array/solution.cpp
@@ -0,0 +1,22 @@
+// link to the problem:
+#include<iostream>
+#include<vector>
+#include<array>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<sstream>
+#include<iterator>
+#include<queue>
+
+class Solution {
+public:
+    std::vector<int> runningSum(std::vector<int>& nums) {
+      for (int i{1}; i < nums.size(); i++) nums[i] = nums[i] + nums[i - 1];
+      return nums;
+    }
+};
+
+int main(int argc, char** argv) {
+
+}
