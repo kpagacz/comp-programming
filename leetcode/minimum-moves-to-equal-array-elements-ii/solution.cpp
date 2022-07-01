2b6005f268277838d227817620b00b496239acb4
kpagacz <konrad.pagacz@gmail.com>
2022-07-01 09:54:37 +0200

    leetcode-miminum-moves-to-equal-array-elements-ii

---
+++ leetcode/minimum-moves-to-equal-array-elements-ii/solution.cpp
@@ -0,0 +1,25 @@
+// link to the problem: https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+public:
+    int minMoves2(std::vector<int>& nums) {
+
+    }
+};
+
+int main(int argc, char** argv) {}
