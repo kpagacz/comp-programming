0df8f37ab99d0566a0f3e5af5564cb1f7da0824f
kpagacz <konrad.pagacz@gmail.com>
2022-07-02 21:44:09 +0200

    leetcode-integer-to-roman

---
+++ leetcode/integer-to-roman/solution.cpp
@@ -0,0 +1,42 @@
+// link to the problem:
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+ public:
+  std::string intToRoman(int num) {
+    std::string roman;
+    auto romanMapIt = romanMap.rbegin();
+    while (num > 0) {
+      const auto& [value, romanRepresentation] = *romanMapIt;
+      if (value > num)
+        ++romanMapIt;
+      else {
+        roman += romanRepresentation;
+        num -= value;
+      }
+    }
+
+    return roman;
+  }
+
+ private:
+  std::vector<std::pair<int, std::string>> romanMap = {{1, "I"},   {4, "IV"},   {5, "V"},   {9, "IX"},  {10, "X"},
+                                                       {40, "XL"}, {50, "L"},   {90, "XC"}, {100, "C"}, {400, "CD"},
+                                                       {500, "D"}, {900, "CM"}, {1000, "M"}};
+};
+
+int main(int argc, char** argv) {}
