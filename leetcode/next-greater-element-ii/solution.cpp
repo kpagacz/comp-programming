7c57064fca97265fc99524f4169099c889966fa0
kpagacz <konrad.pagacz@gmail.com>
2022-07-04 23:25:13 +0200

    leetcode-next-greater-element-ii

--- leetcode/next-greater-element-ii/solution.cpp
+++ leetcode/next-greater-element-ii/solution.cpp
@@ -15,38 +15,38 @@
 #include <unordered_set>
 #include <vector>
 
+// Runtime: 42 ms, faster than 79.46% of C++ online submissions for Next Greater Element II.
+// Memory Usage: 24 MB, less than 62.69% of C++ online submissions for Next Greater Element II.
+
 class Solution {
  public:
   std::vector<int> nextGreaterElements(std::vector<int>& nums) {
     std::stack<int> monotonicStack;
-    nums.reserve(nums.size() * 2);
-    for (const auto& el : nums) nums.push_back(el);
-    std::unordered_map<int, int> nextGreaterMap;
-    for (int i = 0; i < nums.size(); ++i) {
-      // std::cout << "num: " << nums[i] << '\n';
-      while (!monotonicStack.empty() && nums[i] > nums[monotonicStack.top()]) {
-        nextGreaterMap.insert({monotonicStack.top(), i});
-        monotonicStack.pop();
+    std::vector<int> nextGreaterMap(nums.size(), -1);
+    for (int j = 0; j < 2; ++j)
+      for (int i = 0; i < nums.size(); ++i) {
+        // std::cout << "num: " << nums[i] << '\n';
+        while (!monotonicStack.empty() && nums[i] > nums[monotonicStack.top()]) {
+          nextGreaterMap[monotonicStack.top()] = i;
+          monotonicStack.pop();
+        }
+
+        // for (const auto& [key, value] : nextGreaterMap) std::cout << "key: " << key << " value: " << value << '\n';
+        monotonicStack.push(i);
       }
 
-      // for (const auto& [key, value] : nextGreaterMap) std::cout << "key: " << key << " value: " << value << '\n';
-      monotonicStack.push(i);
+    for (int i = 0; i < nextGreaterMap.size(); ++i) {
+      if (nextGreaterMap[i] != -1) nextGreaterMap[i] = nums[nextGreaterMap[i]];
     }
 
-    std::vector<int> answer;
-    for (int i = 0; i < nums.size() / 2; ++i) {
-      const auto nextIndex = nextGreaterMap.count(i) == 1 ? nextGreaterMap[i] : -1;
-      answer.push_back(nextIndex == -1 ? -1 : nums[nextIndex]);
-    }
-
-    return answer;
+    return nextGreaterMap;
   }
 };
 
 int main(int argc, char** argv) {
   // std::vector<int> v{100, 1, 11, 1, 120, 111, 123, 1, -1, -100};
   // std::vector<int> v{1,2,1};
-  std::vector<int> v{1,2,3,4,3};
+  std::vector<int> v{1, 2, 3, 4, 3};
   Solution s;
   s.nextGreaterElements(v);
 }
