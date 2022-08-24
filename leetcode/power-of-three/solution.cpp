c6358e9959d5366373a4b41ec5113b0d5f49f58a
kpagacz <konrad.pagacz@gmail.com>
2022-08-24 08:41:07 +0200

    leetcode-power-of-three

--- leetcode/power-of-three/solution.cpp
+++ leetcode/power-of-three/solution.cpp
@@ -15,14 +15,14 @@
 #include <unordered_set>
 #include <vector>
 
-// Runtime: 24 ms, faster than 79.84% of C++ online submissions for Power of Three.
-// Memory Usage: 5.9 MB, less than 29.00% of C++ online submissions for Power of Three.
+// Runtime: 12 ms, faster than 97.82% of C++ online submissions for Power of Three.
+// Memory Usage: 5.9 MB, less than 72.69% of C++ online submissions for Power of Three.
 
 constexpr int LARGEST_POWER_OF_3 = 1162261467;
 
 class Solution {
  public:
-  bool isPowerOfThree(int n) { return n == 1 || (n > 0 && LARGEST_POWER_OF_3 % n == 0); }
+  bool isPowerOfThree(int n) { return n > 0 && LARGEST_POWER_OF_3 % n == 0; }
 };
 
 int main(int argc, char** argv) {}
