ca3ef28077d16dd28c0a675e29a20748fa4766e5
Konrad Pagacz <konrad.pagacz@gmail.com>
2023-07-31 17:54:56 +0200

    removed debug from the code

--- leetcode/minimum-ascii-delete-sum-for-two-strings/rust/src/main.rs
+++ leetcode/minimum-ascii-delete-sum-for-two-strings/rust/src/main.rs
@@ -2,6 +2,8 @@
 pub struct Solution {}
 
 impl Solution {
+    /// Runtime 5ms Beats 86.67% of users with Rust
+    /// Memory 2.45mb Beats 80.00%of users with Rust
     pub fn minimum_delete_sum(s1: String, s2: String) -> i32 {
         let mut dp = vec![vec![-1; s2.len() + 1]; s1.len() + 1];
         let mut s1_sum = 0;
@@ -36,12 +38,3 @@ impl Solution {
 fn main() {
     println!("Hello, world!");
 }
-
-[[406, 312, 413, 514, 627] d
- [306, 212, 313, 414, 519] e
- [205, 313, 212, 313, 418] l
- [313, 205, 104, 205, 317] e
- [414, 306, 205, 104, 201] t
- [325, 217, 116, 217, 104] e
- [426, 318, 217, 116, 0]]  0
-    l   e    e    t   0
