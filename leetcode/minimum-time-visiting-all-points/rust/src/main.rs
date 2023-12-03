8070b01bfe3ad1dd5ffa95e9c106e97b1eef93bf
Konrad Pagacz <konrad.pagacz@gmail.com>
2023-12-03 15:25:57 +0100

    leetcode-minimum-time-visiting-all-points

---
+++ leetcode/minimum-time-visiting-all-points/rust/src/main.rs
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/minimum-time-visiting-all-points/
+pub struct Solution {}
+impl Solution {
+    pub fn min_time_to_visit_all_points(points: Vec<Vec<i32>>) -> i32 {
+        points
+            .windows(2)
+            .into_iter()
+            .map(|adjacent| {
+                let (first, second) = (&adjacent[0], &adjacent[1]);
+                let (x_diff, y_diff) = ((first[0] - second[0]).abs(), (first[1] - second[1]).abs());
+                x_diff.min(y_diff) + (x_diff - y_diff).abs()
+            })
+            .sum()
+    }
+}
+fn main() {
+    println!("Hello, world!");
+}
