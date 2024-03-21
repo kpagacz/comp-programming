6b5479a4846552e083874ddbb01df189aeeec0c5
Konrad Pagacz <konrad.pagacz@gmail.com>
2024-03-21 17:03:18 +0100

    leetcode-$(basename $PWD)

---
+++ leetcode/reverse-linked-list/rust/src/main.rs
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/reverse-linked-list/description/
+pub struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let (mut prev, mut head) = (None, head);
+
+        while let Some(mut node) = head {
+            head = node.next;
+            node.next = prev;
+            prev = Some(node);
+        }
+
+        prev
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
