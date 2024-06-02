3bc1fd0f0158017a787557bf2e6059d071046c9f
Konrad Pagacz <konrad.pagacz@gmail.com>
2024-06-02 09:11:05 +0200

    leetcode-reverse-string-2

--- leetcode/reverse-string/src/main.rs
+++ leetcode/reverse-string/src/main.rs
@@ -2,11 +2,15 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn reverse_string(s: &mut Vec<char>) {
+    pub fn reverse_string_old(s: &mut Vec<char>) {
         let n = s.len();
         for i in 0..(n / 2) {
             s.swap(i, n - 1 - i);
         }
+    }
+
+    pub fn reverse_string(s: &mut Vec<char>) {
+        *s = s.iter_mut().rev().map(|c| *c).collect::<Vec<char>>();
     }
 }
 
