81191561fda38f6c020d8c88fc3b075423b2288a
Konrad Pagacz <konrad.pagacz@gmail.com>
2024-09-21 12:11:29 +0200

    leetcode-lexicographical-numbers

---
+++ leetcode/lexicographical-numbers/src/main.rs
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/lexicographical-numbers/
+pub struct Solution;
+
+impl Solution {
+    pub fn lexical_order(n: i32) -> Vec<i32> {
+        let mut answer = Vec::with_capacity(n as usize);
+        fn rec(current: i32, answer: &mut Vec<i32>, max: i32) {
+            answer.push(current);
+
+            if current * 10 <= max {
+                rec(current * 10, answer, max);
+            }
+
+            if current % 10 != 9 && current + 1 <= n {
+                rec(current + 1, answer, max);
+            }
+        }
+        rec(1, &mut answer, n);
+        answer
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
