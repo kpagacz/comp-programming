2fbd8e2810b1e9d55f38fcbffecc6d12fd6c0377
Konrad Pagacz <konrad.pagacz@gmail.com>
2025-12-01 09:46:23 +0100

    leetcode-maximum-running-time-of-n-computers

--- leetcode/maximum-running-time-of-n-computers/rust/src/main.rs
+++ leetcode/maximum-running-time-of-n-computers/rust/src/main.rs
@@ -1,14 +1,13 @@
 // https://leetcode.com/problems/maximum-running-time-of-n-computers/
-pub struct Solution {}
+struct Solution {}
+
+#[allow(dead_code)]
 impl Solution {
     // Turns out you don't even have to sort this array. Just taking a min from target and the actual
     // value of the battery time and summing over there is enough. Then you compare the sum with
     // n * target and if it's larger or equal, the time is possible.
-    pub fn max_run_time(n: i32, mut batteries: Vec<i32>) -> i64 {
-        batteries.sort_unstable();
-        batteries.reverse();
-
-        let (mut left, mut right) = (0_i64, 1_00_000 * 1_000_000_000 + 1);
+    pub fn max_run_time(n: i32, batteries: Vec<i32>) -> i64 {
+        let (mut left, mut right) = (0_i64, 100_000 * 1_000_000_000 + 1);
 
         while left < right {
             let middle = (left + right) / 2;
@@ -23,23 +22,12 @@ impl Solution {
         left - 1
     }
 
-    fn can_run_simultaneously(sorted_batteries: &Vec<i32>, time: i64, computers: i32) -> bool {
-        sorted_batteries
+    fn can_run_simultaneously(batteries: &[i32], time: i64, computers: i32) -> bool {
+        batteries
             .iter()
-            .skip(computers as usize)
-            .map(|&el| el as i64)
+            .map(|battery| (*battery as i64).min(time))
             .sum::<i64>()
-            >= sorted_batteries
-                .iter()
-                .take(computers as usize)
-                .filter_map(|&battery_time| {
-                    if (battery_time as i64) < time {
-                        Some(time - (battery_time as i64))
-                    } else {
-                        None
-                    }
-                })
-                .sum::<i64>()
+            >= (computers as i64) * time
     }
 }
 fn main() {
