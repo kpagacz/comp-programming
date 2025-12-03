da826faabd6758198abe010c6aa32e7e131a33e0
Konrad Pagacz <konrad.pagacz@gmail.com>
2025-12-03 07:44:01 +0100

    leetcode-count-number-of-trapezoids-ii

---
+++ leetcode/count-number-of-trapezoids-ii/src/main.rs
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/count-number-of-trapezoids-ii/description/?envType=daily-question&envId=2025-12-03
+struct Solution;
+
+#[allow(dead_code)]
+impl Solution {
+    pub fn count_trapezoids(points: Vec<Vec<i32>>) -> i32 {}
+}
+
+fn main() {
+    println!("Hello, world!");
+}
