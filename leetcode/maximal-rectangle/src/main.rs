9591f6262353f68f0fb69f48e094c6e0b7e45bd8
Konrad Pagacz <konrad.pagacz@gmail.com>
2026-01-11 09:09:02 +0100

    leetcode-maximal-rectangle

--- leetcode/maximal-rectangle/src/main.rs
+++ leetcode/maximal-rectangle/src/main.rs
@@ -1,57 +1,48 @@
 // https://leetcode.com/problems/maximal-rectangle/description/
-pub struct Solution;
+struct Solution;
+
+#[allow(dead_code)]
 impl Solution {
     pub fn maximal_rectangle(matrix: Vec<Vec<char>>) -> i32 {
-        let column_prefixes = Solution::get_column_prefixes(&matrix);
-
-        let mut max_area = 0;
-
-        for row in &column_prefixes {
-            max_area = max_area.max(Solution::max_area_in_a_row(row));
-        }
-
-        max_area as _
-    }
-
-    fn get_column_prefixes(matrix: &[Vec<char>]) -> Vec<Vec<usize>> {
-        let mut column_prefixes = vec![vec![0; matrix[0].len() + 1]; matrix.len()];
+        let mut col_prefixes = vec![vec![0; matrix[0].len() + 1]; matrix.len()];
 
         for i in 0..matrix.len() {
             for j in 0..matrix[0].len() {
                 if matrix[i][j] == '1' {
-                    column_prefixes[i][j] = 1;
                     if i > 0 {
-                        column_prefixes[i][j] += column_prefixes[i - 1][j];
+                        col_prefixes[i][j] = col_prefixes[i - 1][j] + 1;
+                    } else {
+                        col_prefixes[i][j] = 1;
                     }
-                } else {
-                    column_prefixes[i][j] = 0;
                 }
             }
         }
 
-        column_prefixes
-    }
-
-    fn max_area_in_a_row(col_prefixes: &[usize]) -> usize {
-        let mut answer = 0;
         let mut monotonic_stack = vec![];
-
-        for (pos, &ones) in col_prefixes.iter().enumerate() {
-            while !monotonic_stack.is_empty()
-                && ones < col_prefixes[*monotonic_stack.last().unwrap()]
-            {
-                let last_index = monotonic_stack.pop().unwrap();
-                let height = col_prefixes[last_index];
-                let width = match monotonic_stack.last() {
-                    Some(before_last_index) => pos - before_last_index - 1,
-                    None => pos,
-                };
-                answer = answer.max(height * width);
+        let mut max_area = 0;
+        for row in col_prefixes {
+            for (pos, &col_height) in row.iter().enumerate() {
+                while !monotonic_stack.is_empty()
+                    && col_height < row[*monotonic_stack.last().unwrap()]
+                {
+                    let popped_height_index = monotonic_stack.pop().unwrap();
+                    let rectangle_height = row[popped_height_index];
+                    let rectangle_width = match monotonic_stack.last() {
+                        // indices 0,1,2 => width should be 1, because 2 is added, 1 is considered
+                        // and it's just 1 that has the required height
+                        Some(before_popped_height_index) => pos - before_popped_height_index - 1,
+                        // indices 1,2 => width should be 2, because 2 is added, 1 is considered
+                        // and 0 and 1 have the required height
+                        None => pos,
+                    };
+                    max_area = max_area.max(rectangle_height * rectangle_width);
+                }
+                monotonic_stack.push(pos);
             }
-            monotonic_stack.push(pos);
+            monotonic_stack.clear();
         }
 
-        answer
+        max_area as _
     }
 }
 
