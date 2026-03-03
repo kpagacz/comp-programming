21f70255acceddcbaba5aec400225fb11419a503
Konrad Pagacz <konrad.pagacz@gmail.com>
2026-03-03 10:01:00 +0100

    leetcode-find-kth-bit-in-nth-binary-string

--- leetcode/find-kth-bit-in-nth-binary-string/src/main.rs
+++ leetcode/find-kth-bit-in-nth-binary-string/src/main.rs
@@ -2,7 +2,7 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn find_kth_bit(n: i32, k: i32) -> char {
+    pub fn find_kth_bit_old(n: i32, k: i32) -> char {
         let mut s = vec!['0'];
 
         fn invert(s: &[char]) -> Vec<char> {
@@ -17,12 +17,40 @@ impl Solution {
 
         s[(k - 1) as usize]
     }
+
+    pub fn find_kth_bit(_n: i32, k: i32) -> char {
+        fn find_kth_bit_rec(bit: usize) -> char {
+            if bit == 1 {
+                return '0';
+            }
+            if bit.count_ones() == 1 {
+                return '1';
+            }
+
+            let lower_power_of_two = (bit as f64).log2().floor() as u32;
+            let power_of_two = 2usize.pow(lower_power_of_two + 1);
+            let inversed = find_kth_bit_rec(power_of_two - bit);
+
+            match inversed {
+                '0' => '1',
+                '1' => '0',
+                _ => unreachable!(),
+            }
+        }
+
+        let k = k as usize;
+        find_kth_bit_rec(k)
+    }
 }
 
 fn main() {
     let test_cases = [(3, 1), (4, 11)];
 
     for (n, k) in test_cases {
-        println!("{}", Solution::find_kth_bit(n, k));
+        println!(
+            "old {} new {}",
+            Solution::find_kth_bit_old(n, k),
+            Solution::find_kth_bit(n, k)
+        );
     }
 }
