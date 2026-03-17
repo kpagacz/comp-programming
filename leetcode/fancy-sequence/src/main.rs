467272ab525b0f8a2ec1a1950423de79961a6c7d
Konrad Pagacz <konrad.pagacz@gmail.com>
2026-03-17 09:07:17 +0100

    leetcode-largest-submatrix-with-rearrangements

---
+++ leetcode/fancy-sequence/src/main.rs
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/fancy-sequence/description/?envType=daily-question&envId=2026-03-15
+struct Fancy {}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl Fancy {
+    fn new() -> Self {}
+
+    fn append(&self, val: i32) {}
+
+    fn add_all(&self, inc: i32) {}
+
+    fn mult_all(&self, m: i32) {}
+
+    fn get_index(&self, idx: i32) -> i32 {}
+}
+
+/**
+ * Your Fancy object will be instantiated and called as such:
+ * let obj = Fancy::new();
+ * obj.append(val);
+ * obj.add_all(inc);
+ * obj.mult_all(m);
+ * let ret_4: i32 = obj.get_index(idx);
+ */
+fn main() {
+    println!("Hello, world!");
+}
--- leetcode/largest-submatrix-with-rearrangements/rust/src/main.rs
+++ leetcode/largest-submatrix-with-rearrangements/rust/src/main.rs
@@ -1,44 +1,32 @@
-
-// https://leetcode.com/problems/largest-submatrix-with-rearrangements/
-pub struct Solution {}
-
-use std::cmp::Reverse;
+// https://leetcode.com/problems/largest-submatrix-with-rearrangements/description/?envType=daily-question&envId=2026-03-17
+struct Solution;
+#[allow(dead_code)]
 impl Solution {
-    pub fn largest_submatrix(matrix: Vec<Vec<i32>>) -> i32 {
-        // dp[i][j] is the number of consecutive ones in the column j ending at row i
-        let mut dp = vec![vec![0; matrix[0].len()]; matrix.len()];
-
-        for col in 0..matrix[0].len() {
-            let mut ones = 0;
-            for row in 0..matrix.len() {
-                if matrix[row][col] == 1 {
-                    ones += 1;
+    pub fn largest_submatrix(mut matrix: Vec<Vec<i32>>) -> i32 {
+        for row in 0..matrix.len() {
+            for col in 0..matrix[0].len() {
+                if matrix[row][col] == 0 {
+                    matrix[row][col] = 0;
                 } else {
-                    ones = 0;
+                    let prev = if row == 0 { 0 } else { matrix[row - 1][col] };
+                    matrix[row][col] = prev + 1;
                 }
-                dp[row][col] = ones;
             }
         }
-        dp.iter_mut()
-            .for_each(|row| row.sort_unstable_by_key(|el| Reverse(*el)));
-        println!("{dp:?}");
 
-        let mut max = 0;
-        for row in &dp {
-            let mut cols = 0;
-            let mut min_ones = i32::MAX;
-            for &previous_ones in row {
-                cols += 1;
-                min_ones = min_ones.min(previous_ones);
-                max = max.max(cols * min_ones);
-            }
-        }
-        max
+        matrix
+            .into_iter()
+            .map(|mut row| {
+                row.sort_by(|a, b| b.cmp(a));
+                row.into_iter()
+                    .enumerate()
+                    .map(|(pos, val)| (pos as i32 + 1) * val)
+                    .max()
+                    .expect("Matrix is non empty!")
+            })
+            .max()
+            .expect("Matrix is non empty!")
     }
 }
-fn main() {
-    let test_cases = vec![vec![vec![0, 0, 1], vec![1, 1, 1], vec![1, 0, 1]]];
-    for matrix in test_cases {
-        println!("{}", Solution::largest_submatrix(matrix));
-    }
-}
+
+fn main() {}
